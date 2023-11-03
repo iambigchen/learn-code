a9073d65162ba5259bf31019a287401e95bfc58a
鱼仔123 <iambigchen@163.com>
2023-11-03 13:49:21 +0800

    feat:rename

--- this/index.js
+++ this/index.js
@@ -20,3 +20,24 @@ var fn1 = obj.fn1;
 fn1.call(null);
 obj.fn1();
 console.log(window.number);
+
+
+var value = 1;
+
+var foo = {
+  value: 2,
+  bar: function () {
+    return this.value;
+  }
+}
+
+//示例1
+console.log(foo.bar());
+//示例2
+console.log((foo.bar)());
+//示例3
+console.log((foo.bar = foo.bar)());
+//示例4
+console.log((false || foo.bar)());
+//示例5
+console.log((foo.bar, foo.bar)());
